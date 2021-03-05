8aa3c7bd676c45113617f8de755c1f725f31e82a
KVANTORIUM\Преподаватель <dope-boy@yandex.ru>
2021-03-05 19:22:02 +0300

    PlayerCommit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Transform _shootPoint;
+
+    private Weapon _currentWeapon;
+    private int _currentHealth;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _currentHealth = _health;
+        _currentWeapon = _weapons[0];
+    }
+
+    
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _currentWeapon.Shoot(_shootPoint);
+        }
+    }
+}
